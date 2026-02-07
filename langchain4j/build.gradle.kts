40747e6d08348894df661d41ddd29857c5e25a13
Ken Kousen <ken.kousen@kousenit.com>
2026-02-07 17:45:07 -0500

    Update AssertJ to version 3.27.7 in build.gradle.kts

--- langchain4j/build.gradle.kts
+++ langchain4j/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     // Testing
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.assertj:assertj-core:3.27.3")
+    testImplementation("org.assertj:assertj-core:3.27.7")
 }
 
 tasks.withType<Test> {
